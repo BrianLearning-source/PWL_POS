15c583a00715130906ab55ef7b930739d3f6bdce
Brian Serafino Donovan <briandonovan696@gmail.com>
2026-03-10 11:11:10 +0700

    Not Found Exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,15 +10,25 @@ class UserController extends Controller
 {
 
     public function index() {
-        $user = UserModel::findOr(20, ['username', 'nama'], function(){
-            abort(404);
-        });
-
-        return view('user', ['data' => $user]);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        return view('user', ['data' => $user]);   
     }
-  
+    
 
 }
+
+// public function index() {
+    //     $user = UserModel::findOrFail(1);
+    //     return view('user', ['data' => $user]);
+    // }
+
+    // public function index() {
+    //     $user = UserModel::findOr(20, ['username', 'nama'], function(){
+    //         abort(404);
+    //     });
+
+    //     return view('user', ['data' => $user]);
+    // }
 
 // public function index() {
     //     $user = UserModel::findOr(1, ['username', 'nama'], function() {
