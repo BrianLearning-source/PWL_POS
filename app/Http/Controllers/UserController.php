fd1e5e6e15d7f41aef5ba6f7cb8c63136be953a2
Brian Serafino Donovan <briandonovan696@gmail.com>
2026-03-10 10:13:59 +0700

    Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,25 +8,45 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+
     public function index() {
+        $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        });
 
-    $data = [
-        'level_id' => 2,
-        'username' => 'manager_tiga',
-        'nama' => 'Manager 3',
-        'password' => Hash::make('12345')
-    ];
-    UserModel::create($data);
-
-    $user = UserModel::all();
-    return view('user', ['data' => $user]);
-
-    }    
-
+        return view('user', ['data' => $user]);
+    }
+  
 
 }
 
+// public function index() {
+    //     $user = UserModel::findOr(1, ['username', 'nama'], function() {
+    //         abort(404);
+    //     });
 
+    //     return view('user', ['data' => $user]);
+    //}
+
+  // public function index() {
+    //     $user = UserModel::firstWhere('level_id', 1);
+    //     return view('user', ['data' => $user]);
+    // }
+
+   // public function index() {
+
+    // $data = [
+    //     'level_id' => 2,
+    //     'username' => 'manager_tiga',
+    //     'nama' => 'Manager 3',
+    //     'password' => Hash::make('12345')
+    // ];
+    // UserModel::create($data);
+
+    // $user = UserModel::all();
+    // return view('user', ['data' => $user]);
+
+    // }    
 
 
 //public function index() {
